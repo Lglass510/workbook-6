4d526f8e5a01de9a0cb0d5e0e901ee2b1daf572c
Lourie Glass <lourieglass@gmail.com>
2025-05-20 13:41:20 -0500

    final commit

--- WorkingWithInterfaces/src/main/java/Main.java
+++ WorkingWithInterfaces/src/main/java/Main.java
@@ -1,10 +1,23 @@
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        LocalDate date = LocalDate.now();
-        System.out.println(date.getDayOfMonth());
+        List<Person> myFamily = new ArrayList<>();
+        myFamily.add(new Person("SpongeBob", "SquarePants", 35));
+        myFamily.add(new Person("Patrick", "Star", 30));
+        myFamily.add(new Person("Squidward", "Tentacles", 40));
+        myFamily.add(new Person("Eugine", "Krabs", 64));
+        myFamily.add(new Person("Sandy", "Cheeks", 33));
+        Collections.sort(myFamily);
+        for (Person p : myFamily){
+            System.out.println(p.firstName + ", " + p.lastName + ", " + p.age);
+        }
+
+
+
 
     }
 }
--- WorkingWithInterfaces/src/main/java/Person.java
+++ WorkingWithInterfaces/src/main/java/Person.java
@@ -1,39 +1,27 @@
-import java.util.ArrayList;
-import java.util.Collections;
+
 
 public class Person implements Comparable<Person> {
     String firstName;
     String lastName;
     int age;
 
-    @Override
-    public int compareTo(Person other){
-        return this.lastName.compareTo(other.lastName);
-    }
-
-
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
 
+    @Override
+    public int compareTo(Person other){
+        int lastNameCompare = this.lastName.compareToIgnoreCase(other.lastName);
+        if (lastNameCompare != 0) return lastNameCompare;
+
+        int firstNameCompare = this.firstName.compareToIgnoreCase(other.firstName);
+        if (firstNameCompare != 0) return firstNameCompare;
+
+        return Integer.compare(this.age, other.age);
     }
 
 
-    public static void main(String[] args) {
-        ArrayList<Person> myFamily = new ArrayList<>();
-        myFamily.add(new Person("SpongeBob", "SquarePants", 35));
-        myFamily.add(new Person("Patrick", "Star", 30));
-        myFamily.add(new Person("Squidward", "Tentacles", 40));
-        myFamily.add(new Person("Eugine", "Krabs", 64));
-        myFamily.add(new Person("Sandy", "Cheeks", 33));
-
-        Collections.sort(myFamily);
-
-        for (Person p : myFamily){
-            System.out.println(p.firstName + ", " + p.lastName + ", " + p.age);
-        }
-
-    }
 
 }
