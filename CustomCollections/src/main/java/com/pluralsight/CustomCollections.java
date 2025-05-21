354baa140f821b16d1627357bb335c03ed035ae1
Lourie Glass <lourieglass@gmail.com>
2025-05-21 10:19:53 -0500

    final commit

--- CustomCollections/src/main/java/com/pluralsight/CustomCollections.java
+++ CustomCollections/src/main/java/com/pluralsight/CustomCollections.java
@@ -8,7 +8,20 @@ public class CustomCollections {
         shows.add("Living Single");
         shows.add("The Wire");
         shows.add("Avatar:The Last Airbender");
-
         System.out.println(shows.getItems());
+
+        FixedList<Integer> luckyNumbers = new FixedList<>(4);
+        luckyNumbers.add(7);
+        luckyNumbers.add(2);
+        luckyNumbers.add(21);
+        luckyNumbers.add(100);
+        System.out.println(luckyNumbers.getItems());
+
+
+        FixedList<Character> grades = new FixedList<>(3);
+        grades.add('A');
+        grades.add('B');
+        grades.add('C');
+        System.out.println(grades.getItems());
     }
 }
