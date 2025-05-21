856412acdbe2b653e7c1756e902f857794fffefe
Lourie Glass <lourieglass@gmail.com>
2025-05-21 09:44:28 -0500

    final commit

--- Streams/src/main/java/com/pluralsight/streams/Program.java
+++ Streams/src/main/java/com/pluralsight/streams/Program.java
@@ -10,15 +10,15 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         List<Person> people = Arrays.asList(
-                new Person("Harry","Potter", 17),
-                new Person("Hermione", "Granger",17),
+                new Person("Harry", "Potter", 17),
+                new Person("Hermione", "Granger", 17),
                 new Person("Ron", "Weasley", 17),
                 new Person("Draco", "Malfoy", 18),
-                new Person("Luna","Lovegood",15),
-                new Person("Neville", "Longbottom",18),
+                new Person("Luna", "Lovegood", 15),
+                new Person("Neville", "Longbottom", 18),
                 new Person("Severus", "Snape", 45),
-                new Person("Albus","Dumbledoor",77),
-                new Person("Minerva", "McGonagall",74)
+                new Person("Albus", "Dumbledoor", 77),
+                new Person("Minerva", "McGonagall", 74)
         );
 
         System.out.println("Enter a name to search: ");
@@ -27,7 +27,7 @@ public class Program {
         List<Person> matched = new ArrayList<>();
         for (Person person : people) {
             if (person.getFirstName().toUpperCase().contains(inputName) ||
-            person.getLastName().toUpperCase().contains(inputName)) {
+                    person.getLastName().toUpperCase().contains(inputName)) {
                 matched.add(person);
             }
         }
@@ -39,32 +39,26 @@ public class Program {
                 System.out.println(m.getFirstName() + ", " + m.getLastName() + ", " + m.getAge());
             }
 
-    }
-
-    int totalAge = 0;
-    int oldest = Integer.MIN_VALUE;
-    int youngest = Integer.MAX_VALUE;
-
-    for(Person person : people) {
-        int age = person.getAge();
-        totalAge += age;
-
-        if (age > oldest){
-            oldest = age;
         }
-        if(age < youngest){
-            youngest = age;
+
+        int totalAge = 0;
+        int oldest = Integer.MIN_VALUE;
+        int youngest = Integer.MAX_VALUE;
+
+        for (Person person : people) {
+            int age = person.getAge();
+            totalAge += age;
+
+            if (age > oldest) {
+                oldest = age;
+            }
+            if (age < youngest) {
+                youngest = age;
+            }
         }
+
+        double averageAge = (double) totalAge / people.size();
+
+
     }
-
-    double averageAge = (double) totalAge / people.size();
-
-
-
-
-
-
-
-
 }
-
